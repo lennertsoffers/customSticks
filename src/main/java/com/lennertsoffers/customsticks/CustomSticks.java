f003dd9043391fe5a513162c743db2dd2198131b
Lennert Soffers <r0833969@student.thomasmore.be>
2021-08-04 23:40:51 +0200

    added waterBreathing stick functionallity to CustomSticks

--- src/main/java/com/lennertsoffers/customsticks/CustomSticks.java
+++ src/main/java/com/lennertsoffers/customsticks/CustomSticks.java
@@ -3,7 +3,11 @@ package com.lennertsoffers.customsticks;
 import com.lennertsoffers.customsticks.events.ClickEvents;
 import com.lennertsoffers.customsticks.events.PlayerExpChangeEvents;
 import com.lennertsoffers.customsticks.items.ItemManager;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public final class CustomSticks extends JavaPlugin {
 
@@ -12,6 +16,14 @@ public final class CustomSticks extends JavaPlugin {
         ItemManager.init();
         getServer().getPluginManager().registerEvents(new ClickEvents(), this);
         getServer().getPluginManager().registerEvents(new PlayerExpChangeEvents(), this);
+
+        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (player.getInventory().getItemInOffHand().equals(ItemManager.waterBreathingStick) || player.getInventory().getItemInMainHand().equals(ItemManager.waterBreathingStick)) {
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER, 220, 0));
+                }
+            }
+        }, 0L, 200L);
     }
 
     @Override
